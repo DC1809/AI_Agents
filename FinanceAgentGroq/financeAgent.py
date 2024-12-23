e60c0e71e0c2b45d8474562898545cfa5d745f3c
Dipankar <chakrabortydipankar93@gmail.com>
2024-12-23 12:58:12 +0530

    add finance agent

--- FinanceAgentGroq/financeAgent.py
+++ FinanceAgentGroq/financeAgent.py
@@ -3,10 +3,12 @@ from phi.model.groq import Groq
 from phi.tools.duckduckgo import DuckDuckGo
 from phi.tools.yfinance import YFinanceTools
 
+
+api_key='Your_API_Key'
+
 web_agent = Agent(
     name="Web Agent",
-    model=Groq(id="llama-3.3-70b-versatile",api_key='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'),
-    # model=OpenAIChat(id="gpt-4o"),
+    model=Groq(id="llama-3.3-70b-versatile",api_key=api_key),
     tools=[DuckDuckGo()],
     instructions=["Always include sources"],
     show_tool_calls=True,
@@ -16,8 +18,7 @@ web_agent = Agent(
 finance_agent = Agent(
     name="Finance Agent",
     role="Get financial data",
-    model=Groq(id="llama-3.3-70b-versatile",api_key='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'),
-    # model=OpenAIChat(id="gpt-4o"),
+    model=Groq(id="llama-3.3-70b-versatile",api_key=api_key),
     tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, company_info=True)],
     instructions=["Use tables to display data"],
     show_tool_calls=True,
@@ -25,7 +26,7 @@ finance_agent = Agent(
 )
 
 agent_team = Agent(
-    model=Groq(id="llama-3.3-70b-versatile", api_key='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'),
+    model=Groq(id="llama-3.3-70b-versatile", api_key=api_key),
     team=[web_agent, finance_agent],
     instructions=["Always include sources", "Use tables to display data"],
     show_tool_calls=True,
